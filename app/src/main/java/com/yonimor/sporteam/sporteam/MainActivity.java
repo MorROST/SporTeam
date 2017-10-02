34c819f9add55b166ec12776cfa532b48e4faa2e
MORROST-XPS\Mor Aroesti <morrost@gmail.com>
2017-10-02 19:34:51 +0300

    Add login

--- app/src/main/java/com/yonimor/sporteam/sporteam/MainActivity.java
+++ app/src/main/java/com/yonimor/sporteam/sporteam/MainActivity.java
@@ -1,21 +1,15 @@
 package com.yonimor.sporteam.sporteam;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Git works:)");
-        System.out.println("Git works:)");
-        System.out.println("Git works:)");
-        System.out.println("Zobi");
-        System.out.println("Zobiss");
-        System.out.println("i keep on testing");
-        System.out.println("This is Mor's sout");
-        System.out.println("i keep on testing again meening 2");
+
     }
 }
