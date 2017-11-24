f520956b87c207b2785bcd0a754aca4a407d7ae2
MORROST-XPS\Mor Aroesti <morrost@gmail.com>
2017-11-24 16:00:23 +0200

    1.Add validation func in register. 2.Change variable "userName" to "name"  3.Updated string.xml

--- app/src/main/java/com/yonimor/sporteam/sporteam/Register.java
+++ app/src/main/java/com/yonimor/sporteam/sporteam/Register.java
@@ -2,96 +2,198 @@ package com.yonimor.sporteam.sporteam;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.yonimor.sporteam.sporteam.com.data.*;
 
 public class Register extends Activity {
 
 
-    EditText userName, password, phone, email, age;
+    EditText name_editxt, password_editxt,repeatPassword_editxt,phone_editxt, email_editxt, age_editxt;
     RadioGroup group;
-    String gender;
+    String email, password,passwordRepeat, name, gender;
+    String age, phone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
 
-        userName = (EditText) findViewById(R.id.userField);
-        password = (EditText) findViewById(R.id.passField);
-        phone = (EditText) findViewById(R.id.phone_txt_register);
-        email = (EditText) findViewById(R.id.email_txt_register);
-        age = (EditText) findViewById(R.id.age_txt_register);
+        name_editxt = (EditText) findViewById(R.id.name_editxt);
+        password_editxt = (EditText) findViewById(R.id.passField);
+        repeatPassword_editxt = (EditText) findViewById(R.id.repeatPass_editxt);
+        phone_editxt = (EditText) findViewById(R.id.phone_txt_register);
+        email_editxt = (EditText) findViewById(R.id.email_txt_register);
+        age_editxt = (EditText) findViewById(R.id.age_txt_register);
 
         group = (RadioGroup)findViewById(R.id.genderGroup);
         group.check(R.id.maleRBut);
 
     }
-    public boolean ValidCheck()
-    {
-        if (userName.length()<4 || userName.length()>12)
-        {
-            Toast.makeText(this, "User name must be more then 4 char and less than 13 char" , Toast.LENGTH_LONG).show();
-            return false;
-        }
-        else if(password.length()<4 || password.length()>12)
-        {
-            Toast.makeText(this, "Password must be more then 4 char and less than 13 char" , Toast.LENGTH_LONG).show();
-            return false;
+
+    public boolean validations() {
+
+        boolean email_bool = false, password_bool = false, passwordRepeat_bool = false, name_bool = false , age_bool = false, phone_bool = false;  //boolean that check all details are valid
+
+
+        ////////////////////////////////EMAIL ADDRESS///////////////////////////////////
+        final EditText emailValidate = (EditText) findViewById(R.id.email_txt_register);
+
+        //final TextView textView = (TextView) findViewById(R.id.error_lable);                   should enable error label!  next version candidate
+
+        String email = emailValidate.getText().toString().trim();
+
+        String emailPattern = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+";
+
+
+        if (email.matches(emailPattern)) {
+           // textView.setText("");                                                               should enable error label!  next version candidate
+            email_editxt.setBackgroundColor(Color.TRANSPARENT);
+            email_bool = true;
+        } else {
+           // textView.setText("Invalid email address");                                          should enable error label!  next version candidate
+            email_editxt.setBackgroundColor(Color.RED);
+            email_bool = false;
         }
 
-        else if(phone.length()!=10)
+        //////////////////////////////////PASSWORD & REPEAT////////////////////////////////////////
+        if (passwordRepeat.equals("") || password.equals("")) {
+            password_bool = false;
+            passwordRepeat_bool = false;
+            password_editxt.setBackgroundColor(Color.RED);
+            repeatPassword_editxt.setBackgroundColor(Color.RED);
+        } else if (!passwordRepeat.equals("") || !password.equals("")) {
+            password_bool = true;
+            passwordRepeat_bool = true;
+            password_editxt.setBackgroundColor(Color.TRANSPARENT);
+            repeatPassword_editxt.setBackgroundColor(Color.TRANSPARENT);
+
+        }
+        //////////////////////////////////FIRST NAME  //////////////////////////////////
+
+        if (name.equals("")) {
+                name_bool = false;
+                name_editxt.setBackgroundColor(Color.RED);
+
+        }
+        else if (!name.equals(""))
         {
-            Toast.makeText(this, "Phone must be 10 char" , Toast.LENGTH_LONG).show();
+
+                name_editxt.setBackgroundColor(Color.TRANSPARENT);
+                name_bool = true;
+
+        }
+        //////////////////////////////////  AGE  &  PHONE  ////////////////////////////////////////
+        if (age.equals("") || phone.equals("")) {
+            if (phone.equals("")) {
+                phone_editxt.setBackgroundColor(Color.RED);
+                phone_bool = false;
+
+                if(age.equals(""))
+                {
+                    age_editxt.setBackgroundColor(Color.RED);
+                    age_bool = false;
+                }
+            }
+            else {
+                age_editxt.setBackgroundColor(Color.RED);
+                age_bool = false;
+            }
+        }
+        if ((!age.equals("")) || (!phone.equals("")))
+        {
+            if(!phone.equals(""))
+            {
+                phone_editxt.setBackgroundColor(Color.TRANSPARENT);
+                phone_bool = true;
+
+                if(!age.equals(""))
+                {
+                    age_editxt.setBackgroundColor(Color.TRANSPARENT);
+                    age_bool = true;
+                }
+            }
+            else
+            {
+                age_editxt.setBackgroundColor(Color.TRANSPARENT);
+                age_bool = true;
+            }
+        }
+
+
+
+
+        //////////////////////////////////S U M M A R Y/////////////////////////////////////////////
+        if(email_bool && password_bool && passwordRepeat_bool && name_bool && age_bool && phone_bool )
+        {
+
+            return true;
+        }
+        else
+        {
+
             return false;
         }
-
-        else if(1==1)
-        {
-            Toast.makeText(this, "Must validate email" , Toast.LENGTH_LONG).show();
-            //return false;
-        }
-
-        return true;
     }
 
-
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!NOT USING VALIDATION FOR NOW!!
     public void Register(View view) {
-        //boolean check = ValidCheck();
+        fillVariables();
+
         int checkIfWorked = ConnectionData.SOMTHING_WRONG;
-        int selectedID = group.getCheckedRadioButtonId();
-        RadioButton selectedRB = (RadioButton) findViewById(selectedID);
-        gender = selectedRB.getText().toString();
-        //if (check)
-        //{
-            User u = new User(userName.getText().toString(),password.getText().toString(),email.getText().toString(),gender, phone.getText().toString(),Integer.parseInt(age.getText().toString()));
-        checkIfWorked = StartPage.connectionUtil.Register(u);
-        if(checkIfWorked==ConnectionData.OK)
+
+        if (validations())
         {
-            Toast.makeText(this, userName.getText().toString() + " was added" , Toast.LENGTH_LONG).show();
-            finish();
+
+                User u = new User (name, password ,email ,gender , phone,Integer.parseInt(age_editxt.getText().toString()));
+                checkIfWorked = StartPage.connectionUtil.Register(u);
+             if(checkIfWorked==ConnectionData.OK)
+            {
+                Toast.makeText(this, name + " was added" , Toast.LENGTH_LONG).show();
+                finish();
+            }
+            else if(checkIfWorked==ConnectionData.NOT_OK)
+            {
+                Toast.makeText(this, "This email is already registered" , Toast.LENGTH_LONG).show();
+            }
+            else if(checkIfWorked==ConnectionData.SOMTHING_WRONG)
+            {
+                Toast.makeText(this, "There was a problem" , Toast.LENGTH_LONG).show();
+            }
+
         }
-        else if(checkIfWorked==ConnectionData.NOT_OK)
-        {
-            Toast.makeText(this, "This email is already registered" , Toast.LENGTH_LONG).show();
-        }
-        else if(checkIfWorked==ConnectionData.SOMTHING_WRONG)
-        {
-            Toast.makeText(this, "There was a problem" , Toast.LENGTH_LONG).show();
-        }
-        //}
-    }
+
+}
 
     public void back(View view) {
         finish();
         Intent in = new Intent(this, StartPage.class);
         startActivity(in);
+    }
+
+    public void fillVariables() {
+
+
+        int selectedID = group.getCheckedRadioButtonId();
+        RadioButton selectedRB = (RadioButton) findViewById(selectedID);
+        gender = selectedRB.getText().toString();
+
+        email = email_editxt.getText().toString();
+        password = password_editxt.getText().toString();
+        passwordRepeat = repeatPassword_editxt.getText().toString();
+        name = name_editxt.getText().toString();
+        age = age_editxt.getText().toString();
+        phone = phone_editxt.getText().toString();
     }
 }
